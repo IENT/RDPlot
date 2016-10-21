6ee4970ff42424fc969dbf3c7048b6cc080e036e
sauer <sauer@ient.rwth-aachen.de>
2016-10-21 11:43:39 +0200

    first draft of the GUI. Can parse encoder cfg. Display functionalities still missing or broken

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyQt4.uic import loadUiType
+from PyQt4 import QtGui
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import (
@@ -6,6 +7,8 @@ from matplotlib.backends.backend_qt4agg import (
     NavigationToolbar2QT as NavigationToolbar)
 from glob import  glob
 import  re
+import collections
+import numpy as np
 
 Ui_MainWindow, QMainWindow = loadUiType('mainWindow.ui')
 Ui_PlotWidget, QWidget = loadUiType('plotWidget.ui')
@@ -15,16 +18,37 @@ class Main(QMainWindow, Ui_MainWindow):
         super(Main, self).__init__()
         self.setupUi(self)
         self.fig_dict = {}
-        self.sequenceListWidget.itemClicked.connect(self.changefig)
-        self.addSequenceButton.clicked.connect(self.add_sequence)
 
         fig = Figure()
-        self.addmpl(fig)
+        # self.addmpl(fig)
 
-        self.plotWidget = PlotWidget()
-        self.plotAreaVerticalLayout.addWidget(self.plotWidget)
+        self.plotAreaVerticalLayout = QtGui.QVBoxLayout()
+        self.plotsFrame.setLayout(self.plotAreaVerticalLayout)
 
-    def changefig(self, item):
+        # add a widget for previewing plots, they can then be added to the actual plot
+        self.plotPreview = PlotWidget()
+        self.plotAreaVerticalLayout.addWidget(self.plotPreview)
+        # container for all plots
+        self.plotWidgets = []
+        newPlot = PlotWidget()
+        self.plotWidgets.append(newPlot)
+        self.plotAreaVerticalLayout.addWidget(newPlot)
+
+        # store the sequences
+        self.sequences = {}
+
+        # set up signals and slots
+        #self.sequenceListWidget.itemClicked.connect(self.plotPreview.change_plot)
+        self.sequenceListWidget.itemClicked.connect(self.update_preview)
+        self.addSequenceButton.clicked.connect(self.add_sequence)
+        self.addPlotButton.clicked.connect(self.addAnotherPlot)
+
+    def addAnotherPlot(self):
+        newPlot = PlotWidget()
+        self.plotWidgets.append(newPlot)
+        self.plotAreaVerticalLayout.addWidget(newPlot)
+
+    def preview_plot(self, item):
         text = item.text()
         self.rmmpl()
         self.addmpl(self.fig_dict[text])
@@ -33,7 +57,7 @@ class Main(QMainWindow, Ui_MainWindow):
         self.fig_dict[name] = fig
         self.sequenceListWidget.addItem(name)
 
-    def add_sequence(self):
+    def add_sequence(self): # todo: put his logic in its own widget and class, should belong to a listSequencesWidget
         self.filename = QtGui.QFileDialog.getOpenFileNames(
             self,
             "Open Sequence Encoder Log",
@@ -46,63 +70,61 @@ class Main(QMainWindow, Ui_MainWindow):
         sequence_name_common = file_name.rsplit('_QP',1)[0]
         sequence_files = glob(directory + '/' + sequence_name_common + '*')
         sequence = Sequence(sequence_name_common, sequence_files)
+
+        self.sequences[sequence.name] = sequence
+        self.sequenceListWidget.addItem(sequence.name)
+
         pass
 
+    def update_preview(self, item):
+        sequence_name = item.text()
+        sequence = self.sequences[sequence_name]
+        self.plotPreview.change_plot(sequence)
 
-    def addmpl(self, fig):
-        self.canvas = FigureCanvas(fig)
-        self.plotAreaVerticalLayout.addWidget(self.canvas)
-        self.canvas.draw()
-        self.toolbar = NavigationToolbar(self.canvas, 
-                self.plotAreaWidget, coordinates=True)
-        self.plotAreaVerticalLayout.addWidget(self.toolbar)
-# This is the alternate toolbar placement. Susbstitute the three lines above
-# for these lines to see the different look.
-#        self.toolbar = NavigationToolbar(self.canvas,
-#                self, coordinates=True)
-#        self.addToolBar(self.toolbar)
 
-    def rmmpl(self,):
-        self.plotAreaVerticalLayout.removeWidget(self.canvas)
-        self.canvas.close()
-        self.plotAreaVerticalLayout.removeWidget(self.toolbar)
-        self.toolbar.close()
 
+class NestedDict(dict):
+    """
+    Provides the nested dict used to store all the sequence data.
+    """
+    def __getitem__(self, key):
+        if key in self: return self.get(key)
+        return self.setdefault(key, NestedDict())
 
 class PlotWidget(QWidget, Ui_PlotWidget):
     def __init__(self, ):
         super(PlotWidget, self).__init__()
         self.setupUi(self)
         self.fig_dict = {}
-        # self.sequenceListWidget.itemClicked.connect(self.changefig)
+        # self.sequenceListWidget.itemClicked.connect(self.change_plot)
         # self.addSequenceButton.clicked.connect(self.add_sequence)
 
         fig = Figure()
         self.addmpl(fig)
 
-    def changefig(self, item):
-        text = item.text()
+    def change_plot(self, sequence):
+        qp_vals = [int(qp) for qp in sequence.qp_vals]
+        qp_vals.sort()
+        qp_vals.reverse()
+        # np
+        rate = []
+        psnr = []
+        for qp in sequence.qp_vals:
+            rate.append(sequence.summary_data['SUMMARY']['Bitrate'][str(qp)])
+            psnr.append(sequence.summary_data['SUMMARY']['YUV-PSNR'][str(qp)])
+
         self.rmmpl()
-        self.addmpl(self.fig_dict[text])
+
+        fig = Figure()
+        axis = fig.add_subplot(111)
+        axis.plot(rate,psnr)
+
+        self.addmpl(fig)
 
     def addfig(self, name, fig):
         self.fig_dict[name] = fig
         self.sequenceListWidget.addItem(name)
 
-    def add_sequence(self):
-        self.filename = QtGui.QFileDialog.getOpenFileNames(
-            self,
-            "Open Sequence Encoder Log",
-            "/home/ient/Software/rd-plot-gui/examplLogs",
-            "Enocder Logs (*.log)")
-
-        # extract folder and filename
-        [directory, file_name] = self.filename[0].rsplit('/', 1)
-        # extract the part of the filename that files for different QPs share.
-        sequence_name_common = file_name.rsplit('_QP', 1)[0]
-        sequence_files = glob(directory + '/' + sequence_name_common + '*')
-        sequence = Sequence(sequence_name_common, sequence_files)
-        pass
 
     def addmpl(self, fig):
         self.canvas = FigureCanvas(fig)
@@ -119,9 +141,9 @@ class PlotWidget(QWidget, Ui_PlotWidget):
     #        self.addToolBar(self.toolbar)
 
     def rmmpl(self, ):
-        self.plotAreaVerticalLayout.removeWidget(self.canvas)
+        self.verticalLayout.removeWidget(self.canvas)
         self.canvas.close()
-        self.plotAreaVerticalLayout.removeWidget(self.toolbar)
+        self.verticalLayout.removeWidget(self.toolbar)
         self.toolbar.close()
 
 
@@ -145,10 +167,12 @@ class Sequence():
         self.name = ""
         self.qp_vals = []
         self.sequence_files = {} # will fill this with the list sequence_files with qp as key after they are extracted
-        self.name = sequence_name_common
+        self.summary_data = NestedDict()
 
+        self.name = sequence_name_common
         self.extract_qp_vals(sequence_files)
         self.extract_rd_vals()
+
 
     def extract_qp_vals(self, sequence_files):
         for sequence_file in sequence_files:
@@ -162,35 +186,14 @@ class Sequence():
 
 
     def extract_rd_vals(self):
+        """
+        This functions find all data matching the Regex format specified below and stores it in dicts in the sequence.
+        Care was taken to avoid coding explicit names, like 'Y-PSRN', 'YUV-PSNR', etc...
+        """
         for (qp, file) in self.sequence_files.items():
-
             with open(file,'r') as log_file:
                 log_text = log_file.read()  # reads the whole text file
-                # re.search(r"""^SUMMARY.*$' # catch summary line
-                #         # bla
-                #     """, log_text, re.M+re.X).group()
-                # re.search(r"""  ^SUMMARY.*$ # catch summary line
-                #                 \n # catch newline
-                #                 \s*(Total Frames)
-                #            """, log_text, re.M + re.X).group()
-                # re.search(r"""  ^SUMMARY.*$ # catch summary line
-                #                 \s* # catch newline and space
-                #                 (.*)\| # catch phrase Total Frames
-                #                 (\s*(\S*)){5} # catch rest of the line
-                #                 \s* # catch newline and space
-                #
-                #            """, log_text, re.M + re.X).group()
-                #
-                # re.findall(r"""  ^(\w*)-*.*$ # catch summary line
-                #                \s* # catch newline and space
-                #                (.*)\| # catch phrase Total Frames / I / P / B
-                #                (\s*(\S*)){5} # catch rest of the line
-                #                \s* # catch newline and space
-                #                (\d*\s*)\w # catch frame number
-                #                (\s*\d*\.\d*){5} # catch the fractional number (rate, PSNRs)
-                #           """, log_text, re.M + re.X)
-
-                groups = re.findall(r"""  ^(\w*)-*.*$ # catch summary line
+                summaries_qp = re.findall(r"""  ^(\w*)-*.*$ # catch summary line
                                \s* # catch newline and space
                                (.*)\| # catch phrase Total Frames / I / P / B
                                (\s*\S*)(\s*\S*)(\s*\S*)(\s*\S*)(\s*\S*)# catch rest of the line
@@ -199,36 +202,47 @@ class Sequence():
                                (\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*) # catch the fractional number (rate, PSNRs)
                           """, log_text, re.M + re.X)
 
-                # a = re.compile(r"""\d +  # the integral part
-                #    \.    # the decimal point
-                #    \d *  # some fractional digits""", re.X)
-                pass
+                for summary in summaries_qp:
+                    summary_type = summary[0]
+                    names = summary[1:7]
+                    vals = summary[7:]
+
+                    names = [name.strip() for name in names] # remove leading and trailing space
+                    vals = [float(val) for val in vals]  # convert to numbers
+
+                    name_val_dict = dict(zip(names,vals)) # pack both together in a dict
+                    # print(summary_type)
+
+                    # now pack everything together
+                    for name in names:
+                        self.summary_data[summary_type][name][qp] = name_val_dict[name]
+
 
 
 if __name__ == '__main__':
     import sys
     from PyQt4 import QtGui
-    import numpy as np
 
-    fig1 = Figure()
-    ax1f1 = fig1.add_subplot(111)
-    ax1f1.plot(np.random.rand(5))
-
-    fig2 = Figure()
-    ax1f2 = fig2.add_subplot(121)
-    ax1f2.plot(np.random.rand(5))
-    ax2f2 = fig2.add_subplot(122)
-    ax2f2.plot(np.random.rand(10))
-
-    fig3 = Figure()
-    ax1f3 = fig3.add_subplot(111)
-    ax1f3.pcolormesh(np.random.rand(20,20))
+    #
+    # fig1 = Figure()
+    # ax1f1 = fig1.add_subplot(111)
+    # ax1f1.plot(np.random.rand(5))
+    #
+    # fig2 = Figure()
+    # ax1f2 = fig2.add_subplot(121)
+    # ax1f2.plot(np.random.rand(5))
+    # ax2f2 = fig2.add_subplot(122)
+    # ax2f2.plot(np.random.rand(10))
+    #
+    # fig3 = Figure()
+    # ax1f3 = fig3.add_subplot(111)
+    # ax1f3.pcolormesh(np.random.rand(20,20))
 
     app = QtGui.QApplication(sys.argv)
     main = Main()
-    main.addfig('One plot', fig1)
-    main.addfig('Two plots', fig2)
-    main.addfig('Pcolormesh', fig3)
+    # main.addfig('One plot', fig1)
+    # main.addfig('Two plots', fig2)
+    # main.addfig('Pcolormesh', fig3)
     main.show()
     sys.exit(app.exec_())
 
