79916de2998b7cb3c600ef561c32f36f4a9e03cf
Alex Schmidt <alex.schmidt@rwth-aachen.de>
2016-12-07 22:33:25 +0100

    added class ParsedFile in model.py

--- model.py
+++ model.py
@@ -80,6 +80,25 @@ class Sequence():
                                (\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*)(\s*\d*\.\d*) # catch the fractional number (rate, PSNRs)
                           """, log_text, re.M + re.X)
 
+                tempData = re.findall(r""" Total \s Frames \D+
+                        (\d+) \s+ \D \s+ (\S+) \s+ (\S+) # Y-PSNR
+                        \s+ (\S+) \s+ (\S+) \s+ (\S+) # YUV-PSNR
+                        """, log_text, re.M + re.X)
+                frames = []
+                bitrate = []
+                yPsnr = []
+                uPsnr = []
+                vPsnr = []
+                yuvPsnr = []
+
+                for i in range(0, len(tempData)):
+                    frames.append(tempData[i][0])
+                    bitrate.append(tempData[i][1])
+                    yPsnr.append(tempData[i][2])
+                    uPsnr.append(tempData[i][3])
+                    vPsnr.append(tempData[i][4])
+                    yuvPsnr.append(tempData[i][5])
+
                 for summary in summaries_qp:
                     summary_type = summary[0]
                     if summary_type not in self.summary_data:  # create upon first access
@@ -98,3 +117,77 @@ class Sequence():
                         if name not in self.summary_data[summary_type]: # create upon first access
                             self.summary_data[summary_type][name] = []
                         self.summary_data[summary_type][name].append(name_val_dict[name])
+
+class ParsedFile:
+    def __init__(self):
+        self.frames
+        # the S represents summary, in this order: general summary, I,P,B summaries
+        self.yuvPsnrS = []
+        self.yPsnrS = []
+        self.uPsnrS = []
+        self.vPsnrS = []
+        self.bitrateS = []
+
+        # # the I represents I Slices Summary
+        # self.yuvPsnrI = -1
+        # self.yPsnrI = -1
+        # self.uPsnrI = -1
+        # self.vPsnrI = -1
+        # self.bitrateI = -1
+        #
+        # # the P represents P Slices Summary
+        # self.yuvPsnrP = -1
+        # self.yPsnrP = -1
+        # self.uPsnrP = -1
+        # self.vPsnrP = -1
+        # self.bitrateP = -1
+        #
+        # # the B represents B Slices Summary
+        # self.yuvPsnrB = -1
+        # self.yPsnrB = -1
+        # self.uPsnrB = -1
+        # self.vPsnrB = -1
+        # self.bitrateB = -1
+
+        # the T represents temporal Data
+        self.yPsnrT = []
+        self.uPsnrT = []
+        self.vPsnrT = []
+        self.bitsT = []
+        self.sliceT = []
+
+    def extract_temporal_values(self, sequenceName):
+        #this function extracts temporal values
+        with open(sequenceName, 'r') as log_file:
+            log_text = log_file.read()  # reads the whole text file
+            tempData = re.findall(r"""
+                POC \s+ (\d+) \s+ .+ \s+ \d+ \s+ . \s+ (.-\D+) ,  #Slice
+                \s .+ \) \s+ (\d+) \s+ (.+) \s+ \[ (\D+) \s+ (\d+.\d+) \s+ #Y PSNR
+                \D+ \s+ (\D+) \s+ (\d+.\d+) \s+ # U PSNR
+                \D+ \s+ (\D+) \s+ (\d+.\d+) \s+ # v PSNR
+                """, log_text, re.M + re.X)
+
+            for i in range(0, len(tempData)):
+                self.sliceT.append(tempData[i][1])
+                self.bitsT.append(tempData[i][2])
+                self.yPsnrT.append(tempData[i][5])
+                self.uPsnrT.append(tempData[i][7])
+                self.vPsnrT.append(tempData[i][9])
+
+
+    def extract_summary_values(self, sequenceName):
+        # this function extracts temporal values
+        with open(sequenceName, 'r') as log_file:
+            log_text = log_file.read()  # reads the whole text file
+            tempData = re.findall(r""" Total \s Frames \D+
+                        (\d+) \s+ \D \s+ (\S+) \s+ (\S+) # Y-PSNR
+                        \s+ (\S+) \s+ (\S+) \s+ (\S+) # YUV-PSNR
+                        """, log_text, re.M + re.X)
+
+            for i in range(0, len(tempData)):
+                self.frames.append(tempData[i][0])
+                self.bitrateS.append(tempData[i][1])
+                self.yPsnrS.append(tempData[i][2])
+                self.uPsnrS.append(tempData[i][3])
+                self.vPsnrS.append(tempData[i][4])
+                self.yuvPsnrS.append(tempData[i][5])
