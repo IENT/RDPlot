3bcd0078476a45963068686475e8f470cf0f7a88
Jens Schneider <schneider@ient.rwth-aachen.de>
2017-02-11 09:49:14 +0100

    First cleanup for PEP8 on SimDataFactory

--- SimulationDataFactory.py
+++ SimulationDataFactory.py
@@ -8,8 +8,8 @@ import EncLogHM360Lib
 import EncLogHM360LibOld
 import re
 
-class SimulationDataItemFactory():
 
+class SimulationDataItemFactory():
     # this dictionary stores the simulation types in a key value manner,
     # the key is a string which can be parsed from the log file and is unique
     # for a simulation type, the value is the class of the simulation type
@@ -20,7 +20,7 @@ class SimulationDataItemFactory():
                    (r'^HM \s software', EncLogHM.EncLogHM)
                    ]
 
-    def _getSimType(self,path):
+    def _get_sim_type(self, path):
         try:
             for pattern, enclog in self.simTypeDict:
                 with open(path, 'r') as log_file:
@@ -33,21 +33,20 @@ class SimulationDataItemFactory():
 
     # Check whether we have to parse dat directory or encoder logs
     # note, that it is only checked whether we are in a directory called 'dat'
-    def _getLogType(path):
-        if basename(path)=='dat':
-            simDataType = 'dat'
+    def _get_log_type(path):
+        if basename(path) == 'dat':
+            sim_data_type = 'dat'
         else:
-            simDataType = 'encLog'
-        return simDataType
-
+            sim_data_type = 'encLog'
+        return sim_data_type
 
     @classmethod
     def parse_directory(cls, directory_path):
         """Parse a directory for all encoder log files and return generator
            yielding :class: `EncLog`s"""
-        if cls._getLogType(directory_path)=='encLog':
+        if cls._get_log_type(directory_path) == 'encLog':
             paths = glob(join(directory_path, '*_enc.log'))
-            return (cls._getSimType(cls,p)(p) for p in paths)
+            return (cls._get_sim_type(cls, p)(p) for p in paths)
         else:
             print("Dont be foolish. Do something useful here")
 
@@ -64,10 +63,8 @@ class SimulationDataItemFactory():
         # TODO hardcoded file ending, needed to prevent ambiguous occurence
         # exceptions due to *.csv or other files being parsed
         paths = glob(directory + sep + sequence + '*_enc.log')
-        return (cls._getSimType(cls,p)(p) for p in paths)
+        return (cls._get_sim_type(cls, p)(p) for p in paths)
 
     @classmethod
-    def create_instance_for_file(cls,file_path):
-        return cls._getSimType(cls, file_path)(file_path)
-
-
+    def create_instance_for_file(cls, file_path):
+        return cls._get_sim_type(cls, file_path)(file_path)
