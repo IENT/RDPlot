e31edaee3102230a9f3466d11fae4a08183770e4
Jens Schneider <schneider@ient.rwth-aachen.de>
2017-02-11 09:48:54 +0100

    First cleanup for PEP8 on main.py

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ from matplotlib.backends.backend_qt5agg import (
 
 from mpldatacursor import datacursor
 
-
 import numpy as np
 import math
 
 from model import (EncoderLogTreeModel, OrderedDictModel,
                    VariableTreeModel, BdTableModel, dict_tree_from_sim_data_items)
 from view import (EncLogTreeView, QRecursiveSelectionModel)
-
 
 Ui_MainWindow, QMainWindow = loadUiType('mainWindow.ui')
 Ui_PlotWidget, QWidget = loadUiType('plotWidget.ui')
@@ -50,7 +48,7 @@ class Main(QMainWindow, Ui_MainWindow):
 
         # Connect list view with model for the selected values of tree view
         self.selectedSimulationDataItemListModel = OrderedDictModel()
-        self.encoderLogListView.setModel( self.selectedSimulationDataItemListModel )
+        self.encoderLogListView.setModel(self.selectedSimulationDataItemListModel)
         self._selection_model.selectionChanged.connect(self.change_list)
 
         # set up signals and slots
@@ -69,13 +67,13 @@ class Main(QMainWindow, Ui_MainWindow):
             self.encoderLogTreeView.add_folder
         )
         self.actionHide_PlotSettings.triggered.connect(
-            self.setPlotSettingsVisibility
+            self.set_plot_settings_visibility
         )
         self.actionHide_Sequence.triggered.connect(
-            self.setSequenceWidgetVisibility
+            self.set_sequence_widget_visibility
         )
         self.actionHide_Status.triggered.connect(
-            self.setStatusWidgetVisibility
+            self.set_status_widget_visibility
         )
 
         self.actionSave_Table.triggered.connect(
@@ -83,10 +81,10 @@ class Main(QMainWindow, Ui_MainWindow):
         )
 
         self.variableTreeModel = VariableTreeModel()
-        self.variableTreeView.setModel( self.variableTreeModel )
-        self.plotsettings.visibilityChanged.connect(self.plotSettingsVisibilityChanged)
-        self.sequenceWidget.visibilityChanged.connect(self.sequenceWidgetVisibilityChanged)
-        self.statusWidget.visibilityChanged.connect(self.statusWidgetVisibilityChanged)
+        self.variableTreeView.setModel(self.variableTreeModel)
+        self.plotsettings.visibilityChanged.connect(self.plot_settings_visibility_changed)
+        self.sequenceWidget.visibilityChanged.connect(self.sequence_widget_visibility_changed)
+        self.statusWidget.visibilityChanged.connect(self.status_widget_visibility_changed)
 
         # Set recursive selection model for variable view
         self._variable_tree_selection_model = QRecursiveSelectionModel(
@@ -103,16 +101,16 @@ class Main(QMainWindow, Ui_MainWindow):
         self.combo_rate_psnr.currentIndexChanged.connect(self.on_combo_box)
 
     # sets Visibility for the Plotsettings Widget
-    def setPlotSettingsVisibility(self):
-        self.plotsettings.visibilityChanged.disconnect(self.plotSettingsVisibilityChanged)
+    def set_plot_settings_visibility(self):
+        self.plotsettings.visibilityChanged.disconnect(self.plot_settings_visibility_changed)
         if self.plotsettings.isHidden():
             self.plotsettings.setVisible(True)
         else:
             self.plotsettings.setHidden(True)
-        self.plotsettings.visibilityChanged.connect(self.plotSettingsVisibilityChanged)
+        self.plotsettings.visibilityChanged.connect(self.plot_settings_visibility_changed)
 
     # updates the QAction if Visibility is changed
-    def plotSettingsVisibilityChanged(self):
+    def plot_settings_visibility_changed(self):
         if self.plotsettings.isHidden():
             self.actionHide_PlotSettings.setChecked(True)
         else:
@@ -122,30 +120,30 @@ class Main(QMainWindow, Ui_MainWindow):
         self.encoderLogTreeView.deleteKey.connect(self.remove)
 
     # sets Visibility for the Sequence Widget
-    def setSequenceWidgetVisibility(self):
-        self.sequenceWidget.visibilityChanged.disconnect(self.sequenceWidgetVisibilityChanged)
+    def set_sequence_widget_visibility(self):
+        self.sequenceWidget.visibilityChanged.disconnect(self.sequence_widget_visibility_changed)
         if self.sequenceWidget.isHidden():
             self.sequenceWidget.setVisible(True)
         else:
             self.sequenceWidget.setHidden(True)
-        self.sequenceWidget.visibilityChanged.connect(self.sequenceWidgetVisibilityChanged)
+        self.sequenceWidget.visibilityChanged.connect(self.sequence_widget_visibility_changed)
 
-    def sequenceWidgetVisibilityChanged(self):
+    def sequence_widget_visibility_changed(self):
         if self.sequenceWidget.isHidden():
             self.actionHide_Sequence.setChecked(True)
         else:
             self.actionHide_Sequence.setChecked(False)
 
     # Sets Visibility for the Status Widget
-    def setStatusWidgetVisibility(self):
-        self.statusWidget.visibilityChanged.disconnect(self.statusWidgetVisibilityChanged)
+    def set_status_widget_visibility(self):
+        self.statusWidget.visibilityChanged.disconnect(self.status_widget_visibility_changed)
         if self.statusWidget.isHidden():
             self.statusWidget.setVisible(True)
         else:
             self.statusWidget.setHidden(True)
-        self.statusWidget.visibilityChanged.connect(self.statusWidgetVisibilityChanged)
+        self.statusWidget.visibilityChanged.connect(self.status_widget_visibility_changed)
 
-    def statusWidgetVisibilityChanged(self):
+    def status_widget_visibility_changed(self):
         if self.statusWidget.isHidden():
             self.actionHide_Status.setChecked(True)
         else:
@@ -153,9 +151,9 @@ class Main(QMainWindow, Ui_MainWindow):
 
     def remove(self):
         values = self.selectedSimulationDataItemListModel.values()
-        #List call necessary to avoid runtime error because of elements changing
-        #during iteration
-        self.encoderLogTreeModel.remove( list( values ) )
+        # List call necessary to avoid runtime error because of elements changing
+        # during iteration
+        self.encoderLogTreeModel.remove(list(values))
 
     def change_list(self, q_selected, q_deselected):
         """Extend superclass behavior by automatically adding the values of
@@ -166,7 +164,7 @@ class Main(QMainWindow, Ui_MainWindow):
         q_reselect_indexes = []
         for q_index in self.encoderLogTreeView.selectedIndexes():
             if q_index not in selected_q_indexes:
-                q_reselect_indexes.append( q_index )
+                q_reselect_indexes.append(q_index)
 
         # Find all all values that are contained by selected tree items
         tuples = []
@@ -174,14 +172,14 @@ class Main(QMainWindow, Ui_MainWindow):
             # Add values, ie. encoder logs stored at the item, to the list
             # model.
             encoder_logs = q_index.internalPointer().values
-            tuples.extend( (e.path, e) for e in encoder_logs )
+            tuples.extend((e.path, e) for e in encoder_logs)
 
         # Overwrite all elements in dictionary by selected values
         # Note, that ovrewriting only issues one `updated` signal, and thus,
         # only rerenders the plots one time. Therefore, simply overwriting
         # is much more efficient, despite it would seem, that selectively
         # overwriting keys is.
-        self.selectedSimulationDataItemListModel.clear_and_update_from_tuples( tuples )
+        self.selectedSimulationDataItemListModel.clear_and_update_from_tuples(tuples)
 
     def get_selected_simulation_data_items(self):
         return [self.selectedSimulationDataItemListModel[key] for key in self.selectedSimulationDataItemListModel]
@@ -196,8 +194,8 @@ class Main(QMainWindow, Ui_MainWindow):
         plot_data_collection = []
         for q_index in self.variableTreeView.selectedIndexes():
             item = q_index.internalPointer()
-            if len( item.values ) > 0:
-                plot_data_collection.extend( item.values )
+            if len(item.values) > 0:
+                plot_data_collection.extend(item.values)
 
         return plot_data_collection
 
@@ -223,7 +221,7 @@ class Main(QMainWindow, Ui_MainWindow):
         sim_data_items = self.get_selected_simulation_data_items()
         dict_tree = dict_tree_from_sim_data_items(sim_data_items)
         # Reset variable tree and update it with *dict_tree*
-        self.variableTreeModel.clear_and_update_from_dict_tree( dict_tree )
+        self.variableTreeModel.clear_and_update_from_dict_tree(dict_tree)
 
         # Auto expand variable tree
         self.variableTreeView.expandAll()
@@ -234,9 +232,9 @@ class Main(QMainWindow, Ui_MainWindow):
             try:
                 # Reselect new item corresponding to the previously selected
                 # path
-                item = self.variableTreeModel.get_item_from_path( *path )
+                item = self.variableTreeModel.get_item_from_path(*path)
                 self.variableTreeView.selectionModel().select(
-                    self.variableTreeModel._get_index_from_item( item ),
+                    self.variableTreeModel._get_index_from_item(item),
                     QItemSelectionModel.Select,
                 )
             except KeyError:
@@ -246,7 +244,7 @@ class Main(QMainWindow, Ui_MainWindow):
     def update_plot(self):
         plot_data_collection = self.get_plot_data_collection_from_selected_variables()
 
-        self.plotPreview.change_plot( plot_data_collection )
+        self.plotPreview.change_plot(plot_data_collection)
 
         # update the model for the bd table, note the anchor is always
         # the first config if new simDataItems are selected
@@ -259,19 +257,19 @@ class Main(QMainWindow, Ui_MainWindow):
         # if it is non integer per default the first config is regarded as
         # anchor
         self.bdTableModel.update_table(self.combo_rate_psnr.currentText(),
-                                 self.combo_interp.currentText(), index)
+                                       self.combo_interp.currentText(), index)
 
     def save_bd_table(self):
         if self.bdTableModel.rowCount(self) == 0:
             return
-        filename = QtWidgets.QFileDialog.getSaveFileName(self,'Save Table as')[0]
+        filename = QtWidgets.QFileDialog.getSaveFileName(self, 'Save Table as')[0]
         self.bdTableModel.export_to_latex(filename)
 
     def on_combo_box(self):
         # just update the bd table but do not change the anchor
         self.update_bd_table(-1)
 
-    def clearPlot(self):
+    def clear_plot(self):
         pass
 
 
@@ -281,7 +279,8 @@ class NestedDict(dict):
     """
 
     def __getitem__(self, key):
-        if key in self: return self.get(key)
+        if key in self:
+            return self.get(key)
         return self.setdefault(key, NestedDict())
 
 
@@ -300,8 +299,8 @@ class PlotWidget(QWidget, Ui_PlotWidget):
         # self.plotAreaWidget.canvas.setFocusPolicy(Qt.StrongFocus)
 
         # connect scroll and double click event to canvas
-        self.plotAreaWidget.canvas.mpl_connect('scroll_event', self.onWheel)
-        self.plotAreaWidget.canvas.mpl_connect('button_press_event', self.onDbClick)
+        self.plotAreaWidget.canvas.mpl_connect('scroll_event', self.on_wheel)
+        self.plotAreaWidget.canvas.mpl_connect('button_press_event', self.on_db_click)
 
         self.verticalLayout_3.addWidget(self.plotAreaWidget.canvas)
         # add the toolbar for the plot
@@ -318,7 +317,7 @@ class PlotWidget(QWidget, Ui_PlotWidget):
             temporal data
         """
 
-        if len( plot_data_collection ) == 0:
+        if len(plot_data_collection) == 0:
             return
 
         # put a subplot into the figure and set the margins a little bit tighter than the defaults
@@ -326,26 +325,26 @@ class PlotWidget(QWidget, Ui_PlotWidget):
         self.plotAreaWidget.fig.clear()
         axis = self.plotAreaWidget.fig.add_subplot(111)
         self.plotAreaWidget.fig.subplots_adjust(left=0.05, right=0.95,
-                            bottom=0.1, top=0.95,
-                            hspace=0.2, wspace=0.2)
+                                                bottom=0.1, top=0.95,
+                                                hspace=0.2, wspace=0.2)
 
         for plot_data in plot_data_collection:
             # Convert list of pairs of strings to two sorted lists of floats
-            values = ( (float(x), float(y)) for (x, y) in plot_data.values)
+            values = ((float(x), float(y)) for (x, y) in plot_data.values)
             sorted_value_pairs = sorted(values, key=lambda pair: pair[0])
-            [xs, ys] = list( zip(*sorted_value_pairs) )
+            [xs, ys] = list(zip(*sorted_value_pairs))
 
             # Create legend from variable path and encoder log identifiers
             legend = " ".join([plot_data.identifiers[0].split('_')[0]] + [plot_data.identifiers[1]] + plot_data.path)
 
             # plot the current plotdata and set the legend
-            curve = axis.plot( xs, ys, '-x',  label=legend )
+            curve = axis.plot(xs, ys, '-x', label=legend)
             axis.legend(loc='lower right')
 
             # add datacurser for the curve
             datacursor(curve)
 
-            #set grid and default y tick in 0.5 spacing
+            # set grid and default y tick in 0.5 spacing
             axis.grid(True)
             start, end = axis.get_ylim()
             start = math.floor(start)
@@ -356,10 +355,10 @@ class PlotWidget(QWidget, Ui_PlotWidget):
 
     # this function enables zoom with mousewheel
     # see also: http://stackoverflow.com/questions/11551049/matplotlib-plot-zooming-with-scroll-wheel
-    def onWheel(self,event):
+    def on_wheel(self, event):
         base_scale = 1.5
         axis = self.plotAreaWidget.fig.gca()
-        if axis.has_data()==False:
+        if not axis.has_data():
             axis.remove()
             return
         # get the current x and y limits
@@ -380,21 +379,22 @@ class PlotWidget(QWidget, Ui_PlotWidget):
             scale_factor = 1
         # set new limits
         axis.set_xlim([xdata - cur_xrange * scale_factor,
-                     xdata + cur_xrange * scale_factor])
+                       xdata + cur_xrange * scale_factor])
         axis.set_ylim([ydata - cur_yrange * scale_factor,
-                     ydata + cur_yrange * scale_factor])
-        self.plotAreaWidget.canvas.draw() # force re-draw
+                       ydata + cur_yrange * scale_factor])
+        self.plotAreaWidget.canvas.draw()  # force re-draw
 
-    def onDbClick(self,event):
+    def on_db_click(self, event):
         if event.dblclick:
             axis = self.plotAreaWidget.fig.gca()
-            if axis.has_data() == False:
+            if not axis.has_data():
                 axis.remove()
                 return
             axis.autoscale()
             self.plotAreaWidget.canvas.draw()  # force re-draw
         else:
             return
+
 
 if __name__ == '__main__':
     import sys
