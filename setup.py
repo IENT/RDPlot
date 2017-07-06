06fa4857b96b61fc029afcaba81d48d25bb4b47b
Jens Schneider <schneider@ient.rwth-aachen.de>
2017-07-06 14:51:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'rdplot=rdplot:main',
+            'rdplot=rdplot.__main__:main',
         ],
     },
 )
