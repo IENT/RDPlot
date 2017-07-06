519edc6347dc82d00b21b437becf26df6f319e25
Jens Schneider <schneider@ient.rwth-aachen.de>
2017-07-06 15:49:00 +0200

    Add about page to setup.py and MANIFEST

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'rdplot': ['src/rdplot/ui/*','src/rdplot/logo/*','src/rdplot/misc/*'],
+        'rdplot': ['src/rdplot/ui/*','src/rdplot/logo/*','src/rdplot/misc/*','src/rdplot/docs/about.html'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
