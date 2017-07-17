8cfcc31e249734a1f052e771780e20019fecd987
Jens Schneider <schneider@ient.rwth-aachen.de>
2017-07-17 10:50:41 +0200

    Change entry point in setup.py to __main__.py for mac

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-APP = ['src/rdplot/__init__.py']	
+APP = ['src/rdplot/__main__.py']	
 OPTIONS = {'iconfile':'src/rdplot/logo/PLOT1024.icns',
     'plist': {
         'CFBundleName': 'RDPlot',
