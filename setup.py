e7f4dd1089225e48b33f2bc4cc9199fbc7a5f90f
timclassen <tim.classen1@gmx.de>
2026-02-26 17:57:37 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -3,30 +3,35 @@
 """A setuptools based setup module.
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
+from setuptools import setup
 from codecs import open
 import platform
 import os
-import git 
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+# GitPython is optional: use it if available, otherwise fall back to version.txt
+try:
+    import git  # type: ignore
+except ImportError:
+    git = None
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 APP = ['src/rdplot/__main__.py']
-OPTIONS = {'iconfile': 'src/rdplot/logo/PLOT1024.icns',
-           'plist': {
-               'CFBundleName': 'RDPlot',
-               'CFBundleDisplayName': 'RDPlot',
-               'CFBundleGetInfoString': "Making Sandwiches",
-               'CFBundleIdentifier': "com.metachris.osx.sandwich",
-               'CFBundleVersion': "0.1.0",
-               'CFBundleShortVersionString': "0.1.0"}
-           }
+OPTIONS = {
+    'iconfile': 'src/rdplot/logo/PLOT1024.icns',
+    'plist': {
+        'CFBundleName': 'RDPlot',
+        'CFBundleDisplayName': 'RDPlot',
+        'CFBundleGetInfoString': "Making Sandwiches",
+        'CFBundleIdentifier': "com.metachris.osx.sandwich",
+        'CFBundleVersion': "0.1.0",
+        'CFBundleShortVersionString': "0.1.0",
+    },
+}
 
 
 def get_data_files_with_correct_location():
@@ -35,196 +40,220 @@ def get_data_files_with_correct_location():
         If install is done as root, system wide directories and files
         configured for it are used. Otherwise files are installed in users home directory.
     If install happens on Windows:
-        no data_files necessary"""
+        no data_files necessary
+    """
     data_files = []
 
     if 'Linux' in platform.system():
         if os.geteuid() == 0:
             # install as root or with sudo
-            data_files = [('/usr/share/pixmaps/', ['src/rdplot/logo/PLOT64.png']),
-                          ('/usr/share/applications/', ['src/rdplot/misc/rdplot.desktop'])]
-        elif 'FLATPAK_INSTALL' in os.environ : 
+            data_files = [
+                ('/usr/share/pixmaps/', ['src/rdplot/logo/PLOT64.png']),
+                ('/usr/share/applications/', ['src/rdplot/misc/rdplot.desktop']),
+            ]
+        elif 'FLATPAK_INSTALL' in os.environ:
             # install relative to prefix root
             # do nothing, will be handled by flatpak config
-            # data_files = [('/share/applications/', ['src/rdplot/misc/rdplot.desktop'])]   
-            pass         
+            pass
         else:
             # install as user
             data_files = [
-                (os.path.join(os.path.expanduser('~'), '.local/share/icons/'), ['src/rdplot/logo/PLOT64.png']),
-                (os.path.join(os.path.expanduser('~'), '.local/share/applications/'),
-                 ['src/rdplot/misc/rdplot.desktop'])]
+                (
+                    os.path.join(os.path.expanduser('~'), '.local/share/icons/'),
+                    ['src/rdplot/logo/PLOT64.png'],
+                ),
+                (
+                    os.path.join(os.path.expanduser('~'), '.local/share/applications/'),
+                    ['src/rdplot/misc/rdplot.desktop'],
+                ),
+            ]
 
     return data_files
 
 
+def _read_version_txt():
+    """Read version string from version.txt in the project root, if present."""
+    path = os.path.join(here, 'version.txt')
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            return f.readline().strip()
+    except FileNotFoundError:
+        return None
+
+
 def get_version():
     """
-    Read git tag and version given by environment variable and convert it to a version number.
+    Determine version from git tags (if possible) or from version.txt.
+
     Git describe gives something like
         v1.0.0-158-g6c5be28
-    From the git describe help:
-        The command finds the most recent tag that is reachable from a commit.
-        If the tag points to the commit, then only the tag is shown. Otherwise,
-        it suffixes the tag name with the number of additional commits on top
-        of the tagged object and the abbreviated object name of the most recent
-        commit.
-    We will keep the first two number and replace the last with the number of commits since the tag:
+    We keep the first two numbers and replace the last with the number of commits since the tag:
         v1.0.0-158-g6c5be28 -> v1.0.158
-    :return:
     """
     if 'FLATPAK_INSTALL' in os.environ:
-        f = open('version.txt','r')
-        git_describe = f.readline()
-        f.close()        
-    else: 
-        try:
-            r = git.repo.Repo(here)
-            git_describe = r.git.describe('--tags')
-            f = open('version.txt','w')
-            f.write(git_describe)
-            f.close()
-            # and do it one more for packaging if possible
-            f = open('src/rdplot/version.txt','w')
-            f.write(git_describe)
-            f.close()
-        except (git.InvalidGitRepositoryError):
-            f = open('version.txt','r')
-            git_describe = f.readline()
-            f.close()
+        git_describe = _read_version_txt()
+        if not git_describe:
+            raise RuntimeError("version.txt not found; cannot determine version.")
+    else:
+        git_describe = None
 
-    version = None
+        if git is not None:
+            # Try to get version from git
+            try:
+                repo = git.repo.Repo(here)
+                git_describe = repo.git.describe('--tags').strip()
+
+                # Write out version.txt for future installs (e.g. from an sdist)
+                with open(os.path.join(here, 'version.txt'), 'w', encoding='utf-8') as f:
+                    f.write(git_describe)
+
+                # Also put it into the package itself
+                pkg_version_path = os.path.join(here, 'src', 'rdplot', 'version.txt')
+                with open(pkg_version_path, 'w', encoding='utf-8') as f:
+                    f.write(git_describe)
+            except Exception:
+                # No git repository or git failed – fall back to version.txt
+                git_describe = _read_version_txt()
+        else:
+            # GitPython not available – rely on version.txt
+            git_describe = _read_version_txt()
+
+        if not git_describe:
+            raise RuntimeError(
+                "Cannot determine version: neither GitPython+git metadata nor "
+                "version.txt is available."
+            )
+
+    # Now parse git_describe into a version string
     split_describe = git_describe.split('-')
+
     if len(split_describe) == 1:
+        # direct tag
         if '.' not in git_describe:
-            raise Exception("Tag does not comply to the versioning spec. It should be something like v1.0.0, but is %s"
-                            % git_describe)
+            raise Exception(
+                "Tag does not comply to the versioning spec. "
+                "It should be something like v1.0.0, but is %s" % git_describe
+            )
         version = git_describe
     elif len(split_describe) == 3:
+        # tag-commits-g<sha>
         tag = split_describe[0]
         commits_since_tag = split_describe[1]
 
-        # replace last digit with commits_since_tag
         split_tag = tag.split('.')
         if len(split_tag) == 1:
-            raise Exception("Tag does not comply to the versioning spec. It should be something like v1.0.0, but is %s"
-                            % tag)
+            raise Exception(
+                "Tag does not comply to the versioning spec. "
+                "It should be something like v1.0.0, but is %s" % tag
+            )
+
+        # replace last digit with commits_since_tag
         split_tag[-1] = commits_since_tag
         version = '.'.join(split_tag)
-
     else:
-        raise Exception("Can not handle this type of git describe, there should be either no or two '-'. %s"
-                        % git_describe)
+        raise Exception(
+            "Can not handle this type of git describe, there should be either "
+            "no or two '-'. %s" % git_describe
+        )
 
     return version
 
+
 def get_install_requires():
+    """Runtime dependencies (GitPython is intentionally NOT included)."""
     if 'FLATPAK_INSTALL' in os.environ:
-        install_requires=['cycler', 'matplotlib', 'numpy==1.26.4', 
-                      'py', 'pyparsing', 'pyqt5', 'pytest',
-                      'python-dateutil', 'pytz', 'six', 
-                      'scipy', 
-                      'tabulate', 'mpldatacursor',
-                      'xmltodict', 'jsonpickle', 'git',
-#                      'tikzplotlib', 
-                      'Pillow'],
+        install_requires = [
+            'cycler',
+            'matplotlib',
+            'numpy==1.26.4',
+            'py',
+            'pyparsing',
+            'pyqt5',
+            'pytest',
+            'python-dateutil',
+            'pytz',
+            'six',
+            'scipy',
+            'tabulate',
+            'mpldatacursor',
+            'xmltodict',
+            'jsonpickle',
+            # 'tikzplotlib',
+            'Pillow',
+        ]
     else:
-        install_requires=['cycler', 'matplotlib', 'numpy',
-                      'py', 'pyparsing', 'pyqt5', 'pytest',
-                      'python-dateutil', 'pytz', 'six', 
-                      'scipy', 
-                      'tabulate', 'mpldatacursor','git',
-                      'xmltodict', 'jsonpickle', 
-#                      'tikzplotlib', 
-                      'Pillow'],
+        install_requires = [
+            'cycler',
+            'matplotlib',
+            'numpy',
+            'py',
+            'pyparsing',
+            'pyqt5',
+            'pytest',
+            'python-dateutil',
+            'pytz',
+            'six',
+            'scipy',
+            'tabulate',
+            'mpldatacursor',
+            'xmltodict',
+            'jsonpickle',
+            # 'tikzplotlib',
+            'Pillow',
+        ]
     return install_requires
+
 
 setup(
     app=APP,
     options={'py2app': OPTIONS},
     name='rdplot',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=get_version(),
 
     description='A plot tool for rate distortion curves',
     long_description=long_description,
 
-    # The project's main homepage.
     url='https://github.com/IENT/RDPlot',
 
-    # Author details
     author='Jens Schneider, Johannes Sauer, Christoph Weyer, Alex Schmidt',
     author_email='schneider@ient.rwth-aachen.de',
 
-    # Choose your license
     license='GPL-v3',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Information Analysis',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
     ],
 
-    # What does your project relate to?
     keywords='video-coding bjontegaard-delta rate-distortion-plots',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=['rdplot', 'rdplot.lib', 'rdplot.SimulationDataItemClasses', 'rdplot.Widgets'],
     package_dir={'': 'src'},
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    # py_modules=["my_module"],
     include_package_data=True,
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    
     install_requires=get_install_requires(),
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={},
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
-        'rdplot': ['version.txt','src/rdplot/version.txt', 'src/rdplot/ui/*', 'src/rdplot/logo/*', 'src/rdplot/misc/*', 'src/rdplot/docs/about.html'],
+        # paths are relative to the 'rdplot' package directory (src/rdplot)
+        'rdplot': [
+            'version.txt',
+            'ui/*',
+            'logo/*',
+            'misc/*',
+            'docs/about.html',
+        ],
     },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=get_data_files_with_correct_location(),
 
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'rdplot=rdplot.__main__:main',
